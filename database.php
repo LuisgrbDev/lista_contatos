c865ab42a77a98791f500e3bff6ed0446d2a44f1
luisgrbDev_senac <luisgrbenvindo@gmail.com>
2024-05-22 21:42:15 -0300

    tarefa concluida, exibição pura

--- database.php
+++ database.php
@@ -1,39 +1,35 @@
-<?
-class Database{
+
+<?php
+class Database
+{
     private static $instance = null;
     private $conn;
     private $host = 'localhost';
     private $db = 'contatos';
-    private $user = 'contatos';
+    private $user = 'root';
     private $pass = '';
 
     private function __construct()
     {
-        try
-        {
+        try {
             $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db", $this->user, $this->pass);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch(PDOException $e)
-        {
-            echo 'Connection failed' .$e->getMessage();
+        } catch (PDOException $e) {
+            echo 'Connection failed: ' . $e->getMessage();
         }
     }
 
     // Singleton Design Pattern
-    //https://refactoring.guru/design-patterns/singleton
-
+    // https://refactoring.guru/design-patterns/singleton
     public static function getInstance()
     {
-        if(!self::$instance)
-        {
+        if (!self::$instance) {
             self::$instance = new Database();
         }
         return self::$instance;
-
     }
 
-    public function getConnection(){
+    public function getConnection() {
         return $this->conn;
     }
 }
-?>
