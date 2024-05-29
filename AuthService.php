6da4bed026a6052ca71c16beb971e2d5862cfc01
luisgrbDev_senac <luisgrbenvindo@gmail.com>
2024-05-28 21:55:56 -0300

    LOGIN FUNCIONAL

--- AuthService.php
+++ AuthService.php
@@ -40,4 +40,21 @@ if ($type === "register") {
     }
 } elseif ($type === "login") {
     //login de usuario
+    // receber os dados vindos do html
+    $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+    $password = filter_input(INPUT_POST, "password");
+    //verificar se o cadastro existe
+    $usuarioDAO = new usuarioDAO();
+    $usuario = $usuarioDAO->getByEmail($email);
+
+    //redirecionar o usuario para o index.php autenticado
+    if($usuario && password_verify($password,$usuario->getSenha())){
+        $token = bin2hex(random_bytes(25));
+        // atualizar o token do usuario
+        $usuarioDAO->updateToken($usuario->getId(),$token);
+        $_SESSION['token'] = $token;
+        header('Location: index.php');
+    } else{
+        echo "Email ou Senha inválidados";
+    }
 }
